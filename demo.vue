e0cfb4158dbc090343eae2e75b7f7bc870b02297
zhangyx1124 <34392592+zhangyx1124@users.noreply.github.com>
2021-03-25 17:36:27 +0800

    Update demo.vue

--- demo.vue
+++ demo.vue
@@ -1,1 +1,26 @@
 
+ <el-button  style="margin:0 0 20px 20px;" type="primary"  @click="handleDownload">
+           导出
+        </el-button>
+handleDownload() {
+                import('@/vendor/Export2Excel').then(excel => {
+                    const tHeader = ['注册日期', '用户姓名']//表头
+                    const filterVal = ['registe_time', 'username']//对应的值
+                    const list = this.tableData//要导出的数据
+                    const data = this.formatJson(filterVal, list)
+                    excel.export_json_to_excel({
+                        header: tHeader,
+                        data,
+                    })
+                })
+            },
+//数据的处理
+            formatJson(filterVal, jsonData) {
+                return jsonData.map(v => filterVal.map(j => {
+                    if (j === 'timestamp') {
+                        return parseTime(v[j])
+                    } else {
+                        return v[j]
+                    }
+                }))
+            }
